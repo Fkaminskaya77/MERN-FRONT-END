6088d5a19768fbd18b2c034d5ebe076220b38426
flo777 <fkaminskaya21@gmail.com>
2023-03-23 18:48:09 -0600

    setup of react app and Components folder

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+ fetch("localhost:4000") 
+ .then(res => console.log(res))
+ 
+  return (
+ <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
