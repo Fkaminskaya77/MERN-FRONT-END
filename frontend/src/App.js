80b0644574591493ea9a5520ea564cd452a15c61
treychelle_weathers <treychelle.weathers2017@gmail.com>
2023-03-25 12:58:23 -0400

    rendered image for app.js/home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,13 +7,11 @@ function App() {
  .then()
   return (
  <div className="App">
-     
+     <div className="background"></div>
     </div>
   );
 }
 
 export default App;
 
-//App.js - import nav bar from components directory, also render:
-//img as background
-//date & time
+//App.js - import nav bar from components directory & date / time
---
+++ frontend/src/Components/nav.js
@@ -0,0 +1,1 @@
+//nav bar > pages: notes, tasks  [REACT ROUTER]
