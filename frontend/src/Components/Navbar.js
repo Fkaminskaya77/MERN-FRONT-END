a712d3b005c28105b544291960747e86ce1bd59b
Erione White <erionewhite1999@gmail.com>
2023-03-26 15:45:48 -0500

    created navbar, updated styleshee

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import homeImg from '../images/home.png'
 import notesImg from '../images/paper.png'
-import tasksImg from '../images/stickyNote.png'
+import tasksImg from '../images/sticky-note.png'
 
-export default function Navbar() {
+export default function Navbar({ homeLink, notesLink, tasksLink }) {
   return (
     <div className='navbar--container'>
       <ul className='nav-links'>
-        <li className='nav_links__li'>
+        <li className='nav-links__li'>
             <img src={homeImg} alt="house"></img>
-            <a href={props.homeLink}>Home</a>
+            <a href={homeLink}>Home</a>
         </li>
-        <li className='nav_links__li'>
+        <li className='nav-links__li'>
             <img src={notesImg} alt="paper"></img>
-            <a href={props.homeLink}>Notes</a>
+            <a href={notesLink}>Notes</a>
         </li>
-        <li className='nav_links__li'>
+        <li className='nav-links__li'>
             <img src={tasksImg} alt="sticky note"></img>
-            <a href={props.homeLink}>Tasks</a>
+            <a href={tasksLink}>Tasks</a>
         </li>
       </ul>
     </div>
