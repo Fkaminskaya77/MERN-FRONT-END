7dd80bf292c2f62612183d80360633e295e98463
treychelle_weathers <treychelle.weathers2017@gmail.com>
2023-03-26 11:58:15 -0400

    Update Notes.js

--- frontend/src/Components/Notes.js
+++ frontend/src/Components/Notes.js
@@ -9,7 +9,7 @@ export default function Notes() {
     )
 }
 /*
-button LINK TO PROMPT HTML FORM TO COLLECT THEIR DATA, SCHEMA MODELS TO STORE IT, AND RETURN IT BY renderimg all notes on the page.
+button LINK TO PROMPT HTML FORM TO COLLECT THEIR DATA, SCHEMA MODELS TO STORE IT, AND RETURN IT BY renderimg user's JSON (notes) to the page.
 JSON database for user gets created when they save their first note. otherwise the page displays message saying;
 "oh no you haven't jotted any notes yet, click new note below"
 */
