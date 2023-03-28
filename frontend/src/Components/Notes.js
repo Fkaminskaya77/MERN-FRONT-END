4aa1f707ef7b469495030aca805e327cd5ce962e
trey_weathers <treychelle.weathers2017@gmail.com>
2023-03-28 19:38:02 -0400

    minor change

--- frontend/src/Components/Notes.js
+++ frontend/src/Components/Notes.js
@@ -78,5 +78,4 @@ const postsRenderer = posts.map((post) => (
 </div>
 ));
 
-//3/28 check in with Florense and Margo - 
-//NEXT STEPS: GIT ORIGIN PULL, connect to REACT scripts?, commit & merge what I have done/copy to tasks, margot and florense make final  pulls, i start CRUD
+//3/28 check in with Florense and Margo - connect to REACT scripts?, margot and florense make final  pulls/copy to tasks, i start CRUD 
--- frontend/src/Controllers/Notes.js
+++ frontend/src/Controllers/Notes.js
@@ -1,4 +1,1 @@
-/*
-3/28 check in with Florense and Margo - 
-NEXT STEPS: GIT ORIGIN PULL, connect to REACT scripts?, commit & merge what I have done/copy to tasks, margot and florense make final  pulls, i start CRUD
-*/
+
