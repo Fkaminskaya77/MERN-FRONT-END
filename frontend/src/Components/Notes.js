b38b5da2c422c06218d674f7b3d2acbc021556bc
trey_weathers <treychelle.weathers2017@gmail.com>
2023-03-28 14:17:06 -0400

    final steps for components

--- frontend/src/Components/Notes.js
+++ frontend/src/Components/Notes.js
@@ -2,14 +2,17 @@ import react from 'react';
 
 
 export default function Notes() {
+    const json = fetch("https://mern-dashboard-grp3.herokuapp.com/notes")
     Note()
-    //JSON database for user gets created when they save their first note, otherwise the page displays mesage
     return (
     <div>
         <button type="button" onClick="Note"> NEW NOTE </button>
     </div>
     )
+
 }
+console.log(json)
+
 
 function Note() {
     return (
@@ -31,7 +34,7 @@ function Note() {
                     <button 
                         type = "submit"
                         class = "btn"
-                        onClick = "saveNote(event)"
+                        onClick = "Notes"
                         >
                     SAVE NOTE
                     </button>
@@ -41,3 +44,20 @@ function Note() {
         </div>
     )
 }
+
+/*
+
+1.
+*USE MOCK FIRST TO GET COMPONENT WORKING AND TESTED & JSON RENDERED THEN ADD USER INTERACTION W/ BACKEND:
+1. HTML form will be unfunctional - when user clicks submit it will render fake/MOCK JSON to page - not what the user entered saveNote(event) is instead going to invoke the main component rendering fake JSON
+
+
+2. fetch JSON API from the backend. BUT No real USER interaction/data storage/JSON getting from the user to be returned.
+https://mern-dashboard-grp3.herokuapp.com/
+https://mern-dashboard-grp3.herokuapp.com/notes
+https://mern-dashboard-grp3.herokuapp.com/tasks
+3. RENDER MOCK JSON TO PAGE*
+
+2.
+CRUD
+*/
