c53122c6d305f4b586b9ba6b22e3beecae586023
flo777 <fkaminskaya21@gmail.com>
2023-03-29 18:47:46 -0600

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,30 @@
 import "./App.css";
-import Tasks from "./Components/Tasks";
-import Clock from "./Components/homepage";
+import Tasks from "./Components/Tasks.js";
+import Clock from "./Components/homepage.js";
+import React,{useState, useEffect} from "react";
 
 function App() {
-  fetch("localhost:4000")
-    .then((res) => console.log(res))
-    .then();
-  return (
-    <div className="App">
+    //API URL 
+    const URL = "https://mern-dashboard-grp3.herokuapp.com/";
+    const [Task, setTask] = useState([])
+  
+      //Fetching Data from API
+    function getTask () {
+      fetch(URL)
+      
+    .then(response => response.json())
+    .then(data => setTask(data))
+  }
+    useEffect(getTask, []);
+    console.log(Task)
+ 
+    return (
+    <main className="App">
       <div className="background"></div>
       <Tasks />
       <Clock />
-    </div>
+     
+    </main>
   );
 }
 
--- frontend/src/Components/Tasks.js
+++ frontend/src/Components/Tasks.js
@@ -1,4 +1,37 @@
-/*
-Tasks - render all tasks
-    ^same thing as notes but different schema/model
-*/
+import react from 'react';
+
+
+function Tasks() {
+
+    return(
+        <main className= 'tasks'>
+             <div className="taskPage">
+            
+                <h3>PlaceHolder:Title</h3>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <p>PlaceHolder:URGENCY</p>
+        
+        <div className='content__icons'>
+            
+            <button><i class="fa-solid fa-trash"></i></button>
+            <button><i class="fa-regular fa-pen-to-square"></i></button>
+            
+            <button>NEW TASK +</button>
+        </div>
+            
+              </div>
+
+        </main>
+        
+    )
+}
+
+export default Tasks;
+
+
+
