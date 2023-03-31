4430e7db8fcf9006b2a151e8074faf29b8ac8c75
Erione White <erionewhite1999@gmail.com>
2023-03-31 16:43:05 -0500

    commit before merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
 
+
 function App() {
  fetch("localhost:4000") 
  .then(res => console.log(res))
  .then()
   return (
  <div className="App">
-     
+    
+    <button className='btn--gray'>New Note</button>
     </div>
   );
 }
