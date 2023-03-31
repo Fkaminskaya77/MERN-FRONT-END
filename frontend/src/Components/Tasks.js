e7e433a959ad980c1c1cad420a120407e92cdc03
margot_shupe <jmargotromov@gmail.com>
2023-03-30 21:48:29 -0400

    conditionals done

--- frontend/src/Components/Tasks.js
+++ frontend/src/Components/Tasks.js
@@ -4,17 +4,42 @@ function Tasks({ Task }) {
   return (
     <main className="tasks--page">
       <div className="content--container">
-        {Task.map((task, index) => (
-          <div key={index} className="content--container">
-            <h3 className="content__Title">{task.title}</h3>
-            <ul>
-              {task.toDos.map((toDos, index) => {
-                return <li key={index}>{toDos}</li>;
-              })}
-            </ul>
-            <p>PlaceHolder:URGENCY</p>
-          </div>
-        ))}
+        {Task.map((task, index) => {
+          if (task.urgency == "low") {
+            return (
+              <div key={index} className="content--container low-urgency">
+                <h3 className="content__Title">{task.title}</h3>
+                <ul>
+                  {task.toDos.map((toDos, index) => {
+                    return <li key={index}>{toDos}</li>;
+                  })}
+                </ul>
+              </div>
+            );
+          } else if (task.urgency == "medium") {
+            return (
+              <div key={index} className="content--container med-urgency">
+                <h3 className="content__Title">{task.title}</h3>
+                <ul>
+                  {task.toDos.map((toDos, index) => {
+                    return <li key={index}>{toDos}</li>;
+                  })}
+                </ul>
+              </div>
+            );
+          } else if (task.urgency == "high") {
+            return (
+              <div key={index} className="content--container high-urgency">
+                <h3 className="content__Title">{task.title}</h3>
+                <ul>
+                  {task.toDos.map((toDos, index) => {
+                    return <li key={index}>{toDos}</li>;
+                  })}
+                </ul>
+              </div>
+            );
+          }
+        })}
         <div className="content__icons">
           <button>
             <i className="fa-solid fa-trash"></i>
