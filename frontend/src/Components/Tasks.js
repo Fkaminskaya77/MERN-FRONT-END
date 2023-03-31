2f16e595d56f5e6ea37157a439aa579882edf201
margot_shupe <jmargotromov@gmail.com>
2023-03-30 21:13:13 -0400

    showing data on task

--- frontend/src/Components/Tasks.js
+++ frontend/src/Components/Tasks.js
@@ -1,41 +1,33 @@
-import react from 'react';
+import react from "react";
 
-
-function Tasks({Task}) {
-
-    return(
-        <main className= 'tasks--page'>
-             <div className="content--container">
-            
-             {Task.map((task, index) => (
-                 <div key={index} className="content--container">
-                
-                <h3 className='content__Title'>{task.title}</h3>
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-                <p>PlaceHolder:URGENCY</p>
-                </div>
+function Tasks({ Task }) {
+  return (
+    <main className="tasks--page">
+      <div className="content--container">
+        {Task.map((task, index) => (
+          <div key={index} className="content--container">
+            <h3 className="content__Title">{task.title}</h3>
+            <ul>
+              {task.toDos.map((toDos, index) => {
+                return <li key={index}>{toDos}</li>;
+              })}
+            </ul>
+            <p>PlaceHolder:URGENCY</p>
+          </div>
         ))}
-        <div className='content__icons'>
-            
-            <button><i className="fa-solid fa-trash"></i></button>
-            <button><i className="fa-regular fa-pen-to-square"></i></button>
-            
-            <button>NEW TASK +</button>
-        </div>
-            
-              </div>
+        <div className="content__icons">
+          <button>
+            <i className="fa-solid fa-trash"></i>
+          </button>
+          <button>
+            <i className="fa-regular fa-pen-to-square"></i>
+          </button>
 
-        </main>
-        
-    )
+          <button>NEW TASK +</button>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Tasks;
-
-
-
