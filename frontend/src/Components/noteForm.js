faf2fbe51f1f688b3d8ebd638cd63ab84f635460
flo777 <fkaminskaya21@gmail.com>
2023-04-02 12:33:00 -0600

    doing button submit test

--- frontend/src/Components/noteForm.js
+++ frontend/src/Components/noteForm.js
@@ -1,15 +1,28 @@
 import React from "react";
+import { useState } from "react";
  
 
 function NoteForm({handleBtnClick}) {
-  
-  return(
+  // const url = ""
+const [data, setData] = useState({
+  title: "",
+  body:"",
+})
+
+function handleChange(e) {
+const newData = {...data}
+newData[e.target.id] = e.target.value
+setData(newData)
+console.log(newData)
+}
+
+return(
     
     <div>
       <form>
 
-        <input type="text" placeholder="title"></input>
-        <textarea></textarea>
+        <input onChange={(e)=> handleChange(e)} id="title" value={data.title} type="text" placeholder="title"></input>
+        <textarea onChange={(e)=> handleChange(e)} id="body" value={data.body} placeholder= "Add your note here.."></textarea>
         <button className="close_button" onClick={handleBtnClick}>x</button>
         <button>Submit</button>
       </form>
